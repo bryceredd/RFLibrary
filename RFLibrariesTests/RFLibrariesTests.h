427f4a5bd789e791142eca21f865cc822bd4fae3
bryce <bryce@i.tv>
2012-04-19 23:13:17 -0600

    added initial commit

---
+++ RFLibrariesTests/RFLibrariesTests.h
@@ -0,0 +1,13 @@
+//
+//  RFLibrariesTests.h
+//  RFLibrariesTests
+//
+//  Created by Bryce Redd on 4/19/12.
+//  Copyright (c) 2012 Itv. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RFLibrariesTests : SenTestCase
+
+@end
